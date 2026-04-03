3fd5120177c4781275b3da14a1de3949a1c954e0
Enrique Miceli <enrique.miceli@gmail.com>
2026-04-03 00:20:24 -0600

    fix(grafo): convert nashville/kitchener-waterloo/mississauga grafos from array to object format
    Fixes prerender crash: 'Cannot read properties of undefined (reading 0)'
    These 3 grafos were exported as arrays instead of keyed objects.
    Spreading arrays into grafoCombinado created numeric index keys
    instead of slug keys, breaking station lookup during static generation.

--- data/kitchener-waterloo/grafo.js
+++ data/kitchener-waterloo/grafo.js
@@ -1,108 +1,108 @@
-export const grafoKitchenerWaterloo = [
-  {
-    ciudad: "Kitchener-Waterloo",
-    nombre: "Conestoga Station",
-    lineas: ["ion-red"],
+export const grafoKitchenerWaterloo = {
+  'kitchener-waterloo-conestoga': {
+    ciudad: 'kitchener-waterloo',
+    nombre: 'Conestoga Station',
+    lineas: ['ion-red'],
     adyacentes: [
-      { slug: "kitchener-waterloo-conestoga-mall", tiempo: 3, linea: "ion-red", tipo: "lrt" }
+      { slug: 'kitchener-waterloo-conestoga-mall', tiempo: 3, linea: 'ion-red', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Kitchener-Waterloo",
-    nombre: "Conestoga Mall Station",
-    lineas: ["ion-red"],
+  'kitchener-waterloo-conestoga-mall': {
+    ciudad: 'kitchener-waterloo',
+    nombre: 'Conestoga Mall Station',
+    lineas: ['ion-red'],
     adyacentes: [
-      { slug: "kitchener-waterloo-conestoga", tiempo: 3, linea: "ion-red", tipo: "lrt" },
-      { slug: "kitchener-waterloo-ottawa", tiempo: 3, linea: "ion-red", tipo: "lrt" }
+      { slug: 'kitchener-waterloo-conestoga', tiempo: 3, linea: 'ion-red', tipo: 'lrt' },
+      { slug: 'kitchener-waterloo-ottawa', tiempo: 3, linea: 'ion-red', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Kitchener-Waterloo",
-    nombre: "Ottawa Station",
-    lineas: ["ion-red"],
+  'kitchener-waterloo-ottawa': {
+    ciudad: 'kitchener-waterloo',
+    nombre: 'Ottawa Station',
+    lineas: ['ion-red'],
     adyacentes: [
-      { slug: "kitchener-waterloo-conestoga-mall", tiempo: 3, linea: "ion-red", tipo: "lrt" },
-      { slug: "kitchener-waterloo-fischer-hallman", tiempo: 3, linea: "ion-red", tipo: "lrt" }
+      { slug: 'kitchener-waterloo-conestoga-mall', tiempo: 3, linea: 'ion-red', tipo: 'lrt' },
+      { slug: 'kitchener-waterloo-fischer-hallman', tiempo: 3, linea: 'ion-red', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Kitchener-Waterloo",
-    nombre: "Fischer-Hallman Station",
-    lineas: ["ion-red"],
+  'kitchener-waterloo-fischer-hallman': {
+    ciudad: 'kitchener-waterloo',
+    nombre: 'Fischer-Hallman Station',
+    lineas: ['ion-red'],
     adyacentes: [
-      { slug: "kitchener-waterloo-ottawa", tiempo: 3, linea: "ion-red", tipo: "lrt" },
-      { slug: "kitchener-waterloo-chandler", tiempo: 3, linea: "ion-red", tipo: "lrt" }
+      { slug: 'kitchener-waterloo-ottawa', tiempo: 3, linea: 'ion-red', tipo: 'lrt' },
+      { slug: 'kitchener-waterloo-chandler', tiempo: 3, linea: 'ion-red', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Kitchener-Waterloo",
-    nombre: "Chandler Station",
-    lineas: ["ion-red"],
+  'kitchener-waterloo-chandler': {
+    ciudad: 'kitchener-waterloo',
+    nombre: 'Chandler Station',
+    lineas: ['ion-red'],
     adyacentes: [
-      { slug: "kitchener-waterloo-fischer-hallman", tiempo: 3, linea: "ion-red", tipo: "lrt" },
-      { slug: "kitchener-waterloo-kitchener", tiempo: 3, linea: "ion-red", tipo: "lrt" }
+      { slug: 'kitchener-waterloo-fischer-hallman', tiempo: 3, linea: 'ion-red', tipo: 'lrt' },
+      { slug: 'kitchener-waterloo-kitchener', tiempo: 3, linea: 'ion-red', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Kitchener-Waterloo",
-    nombre: "Kitchener Station",
-    lineas: ["ion-red"],
+  'kitchener-waterloo-kitchener': {
+    ciudad: 'kitchener-waterloo',
+    nombre: 'Kitchener Station',
+    lineas: ['ion-red'],
     adyacentes: [
-      { slug: "kitchener-waterloo-chandler", tiempo: 3, linea: "ion-red", tipo: "lrt" },
-      { slug: "kitchener-waterloo-king-centre", tiempo: 3, linea: "ion-red", tipo: "lrt" }
+      { slug: 'kitchener-waterloo-chandler', tiempo: 3, linea: 'ion-red', tipo: 'lrt' },
+      { slug: 'kitchener-waterloo-king-centre', tiempo: 3, linea: 'ion-red', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Kitchener-Waterloo",
-    nombre: "King Centre",
-    lineas: ["ion-red"],
+  'kitchener-waterloo-king-centre': {
+    ciudad: 'kitchener-waterloo',
+    nombre: 'King Centre',
+    lineas: ['ion-red'],
     adyacentes: [
-      { slug: "kitchener-waterloo-kitchener", tiempo: 3, linea: "ion-red", tipo: "lrt" },
-      { slug: "kitchener-waterloo-market-hall", tiempo: 3, linea: "ion-red", tipo: "lrt" }
+      { slug: 'kitchener-waterloo-kitchener', tiempo: 3, linea: 'ion-red', tipo: 'lrt' },
+      { slug: 'kitchener-waterloo-market-hall', tiempo: 3, linea: 'ion-red', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Kitchener-Waterloo",
-    nombre: "Kitchener Market Hall",
-    lineas: ["ion-red"],
+  'kitchener-waterloo-market-hall': {
+    ciudad: 'kitchener-waterloo',
+    nombre: 'Kitchener Market Hall',
+    lineas: ['ion-red'],
     adyacentes: [
-      { slug: "kitchener-waterloo-king-centre", tiempo: 3, linea: "ion-red", tipo: "lrt" },
-      { slug: "kitchener-waterloo-victoria-park", tiempo: 3, linea: "ion-red", tipo: "lrt" }
+      { slug: 'kitchener-waterloo-king-centre', tiempo: 3, linea: 'ion-red', tipo: 'lrt' },
+      { slug: 'kitchener-waterloo-victoria-park', tiempo: 3, linea: 'ion-red', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Kitchener-Waterloo",
-    nombre: "Victoria Park Station",
-    lineas: ["ion-red"],
+  'kitchener-waterloo-victoria-park': {
+    ciudad: 'kitchener-waterloo',
+    nombre: 'Victoria Park Station',
+    lineas: ['ion-red'],
     adyacentes: [
-      { slug: "kitchener-waterloo-market-hall", tiempo: 3, linea: "ion-red", tipo: "lrt" },
-      { slug: "kitchener-waterloo-university-waterloo", tiempo: 3, linea: "ion-red", tipo: "lrt" }
+      { slug: 'kitchener-waterloo-market-hall', tiempo: 3, linea: 'ion-red', tipo: 'lrt' },
+      { slug: 'kitchener-waterloo-university-waterloo', tiempo: 3, linea: 'ion-red', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Kitchener-Waterloo",
-    nombre: "University of Waterloo",
-    lineas: ["ion-red"],
+  'kitchener-waterloo-university-waterloo': {
+    ciudad: 'kitchener-waterloo',
+    nombre: 'University of Waterloo',
+    lineas: ['ion-red'],
     adyacentes: [
-      { slug: "kitchener-waterloo-victoria-park", tiempo: 3, linea: "ion-red", tipo: "lrt" },
-      { slug: "kitchener-waterloo-columbia", tiempo: 3, linea: "ion-red", tipo: "lrt" }
+      { slug: 'kitchener-waterloo-victoria-park', tiempo: 3, linea: 'ion-red', tipo: 'lrt' },
+      { slug: 'kitchener-waterloo-columbia', tiempo: 3, linea: 'ion-red', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Kitchener-Waterloo",
-    nombre: "Columbia Station",
-    lineas: ["ion-red"],
+  'kitchener-waterloo-columbia': {
+    ciudad: 'kitchener-waterloo',
+    nombre: 'Columbia Station',
+    lineas: ['ion-red'],
     adyacentes: [
-      { slug: "kitchener-waterloo-university-waterloo", tiempo: 3, linea: "ion-red", tipo: "lrt" },
-      { slug: "kitchener-waterloo-ainslie-street", tiempo: 3, linea: "ion-red", tipo: "lrt" }
+      { slug: 'kitchener-waterloo-university-waterloo', tiempo: 3, linea: 'ion-red', tipo: 'lrt' },
+      { slug: 'kitchener-waterloo-ainslie-street', tiempo: 3, linea: 'ion-red', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Kitchener-Waterloo",
-    nombre: "Ainslie Street Terminal",
-    lineas: ["ion-red"],
+  'kitchener-waterloo-ainslie-street': {
+    ciudad: 'kitchener-waterloo',
+    nombre: 'Ainslie Street Terminal',
+    lineas: ['ion-red'],
     adyacentes: [
-      { slug: "kitchener-waterloo-columbia", tiempo: 3, linea: "ion-red", tipo: "lrt" }
+      { slug: 'kitchener-waterloo-columbia', tiempo: 3, linea: 'ion-red', tipo: 'lrt' }
     ]
   }
-];
+};
--- data/mississauga/grafo.js
+++ data/mississauga/grafo.js
@@ -1,153 +1,153 @@
-export const grafoMississauga = [
-  {
-    ciudad: "Mississauga",
-    nombre: "Kipling Station",
-    lineas: ["hazel-mcallion-purple"],
+export const grafoMississauga = {
+  'mississauga-kipling-station': {
+    ciudad: 'mississauga',
+    nombre: 'Kipling Station',
+    lineas: ['hazel-mcallion-purple'],
     adyacentes: [
-      { slug: "mississauga-dundas-west", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" }
+      { slug: 'mississauga-dundas-west', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Mississauga",
-    nombre: "Dundas West",
-    lineas: ["hazel-mcallion-purple"],
+  'mississauga-dundas-west': {
+    ciudad: 'mississauga',
+    nombre: 'Dundas West',
+    lineas: ['hazel-mcallion-purple'],
     adyacentes: [
-      { slug: "mississauga-kipling-station", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" },
-      { slug: "mississauga-burnhamthorpe", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" }
+      { slug: 'mississauga-kipling-station', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' },
+      { slug: 'mississauga-burnhamthorpe', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Mississauga",
-    nombre: "Burnhamthorpe",
-    lineas: ["hazel-mcallion-purple"],
+  'mississauga-burnhamthorpe': {
+    ciudad: 'mississauga',
+    nombre: 'Burnhamthorpe',
+    lineas: ['hazel-mcallion-purple'],
     adyacentes: [
-      { slug: "mississauga-dundas-west", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" },
-      { slug: "mississauga-city-centre", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" }
+      { slug: 'mississauga-dundas-west', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' },
+      { slug: 'mississauga-city-centre', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Mississauga",
-    nombre: "City Centre",
-    lineas: ["hazel-mcallion-purple"],
+  'mississauga-city-centre': {
+    ciudad: 'mississauga',
+    nombre: 'City Centre',
+    lineas: ['hazel-mcallion-purple'],
     adyacentes: [
-      { slug: "mississauga-burnhamthorpe", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" },
-      { slug: "mississauga-square-one", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" }
+      { slug: 'mississauga-burnhamthorpe', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' },
+      { slug: 'mississauga-square-one', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Mississauga",
-    nombre: "Square One",
-    lineas: ["hazel-mcallion-purple"],
+  'mississauga-square-one': {
+    ciudad: 'mississauga',
+    nombre: 'Square One',
+    lineas: ['hazel-mcallion-purple'],
     adyacentes: [
-      { slug: "mississauga-city-centre", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" },
-      { slug: "mississauga-rathburn", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" }
+      { slug: 'mississauga-city-centre', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' },
+      { slug: 'mississauga-rathburn', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Mississauga",
-    nombre: "Rathburn",
-    lineas: ["hazel-mcallion-purple"],
+  'mississauga-rathburn': {
+    ciudad: 'mississauga',
+    nombre: 'Rathburn',
+    lineas: ['hazel-mcallion-purple'],
     adyacentes: [
-      { slug: "mississauga-square-one", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" },
-      { slug: "mississauga-bloor-street", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" }
+      { slug: 'mississauga-square-one', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' },
+      { slug: 'mississauga-bloor-street', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Mississauga",
-    nombre: "Bloor Street",
-    lineas: ["hazel-mcallion-purple"],
+  'mississauga-bloor-street': {
+    ciudad: 'mississauga',
+    nombre: 'Bloor Street',
+    lineas: ['hazel-mcallion-purple'],
     adyacentes: [
-      { slug: "mississauga-rathburn", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" },
-      { slug: "mississauga-cawthra", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" }
+      { slug: 'mississauga-rathburn', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' },
+      { slug: 'mississauga-cawthra', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Mississauga",
-    nombre: "Cawthra",
-    lineas: ["hazel-mcallion-purple"],
+  'mississauga-cawthra': {
+    ciudad: 'mississauga',
+    nombre: 'Cawthra',
+    lineas: ['hazel-mcallion-purple'],
     adyacentes: [
-      { slug: "mississauga-bloor-street", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" },
-      { slug: "mississauga-hurontario-downtown", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" }
+      { slug: 'mississauga-bloor-street', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' },
+      { slug: 'mississauga-hurontario-downtown', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Mississauga",
-    nombre: "Hurontario Downtown",
-    lineas: ["hazel-mcallion-purple"],
+  'mississauga-hurontario-downtown': {
+    ciudad: 'mississauga',
+    nombre: 'Hurontario Downtown',
+    lineas: ['hazel-mcallion-purple'],
     adyacentes: [
-      { slug: "mississauga-cawthra", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" },
-      { slug: "mississauga-lakeshore-road", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" }
+      { slug: 'mississauga-cawthra', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' },
+      { slug: 'mississauga-lakeshore-road', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Mississauga",
-    nombre: "Lakeshore Road",
-    lineas: ["hazel-mcallion-purple"],
+  'mississauga-lakeshore-road': {
+    ciudad: 'mississauga',
+    nombre: 'Lakeshore Road',
+    lineas: ['hazel-mcallion-purple'],
     adyacentes: [
-      { slug: "mississauga-hurontario-downtown", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" },
-      { slug: "mississauga-port-street", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" }
+      { slug: 'mississauga-hurontario-downtown', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' },
+      { slug: 'mississauga-port-street', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Mississauga",
-    nombre: "Port Street",
-    lineas: ["hazel-mcallion-purple"],
+  'mississauga-port-street': {
+    ciudad: 'mississauga',
+    nombre: 'Port Street',
+    lineas: ['hazel-mcallion-purple'],
     adyacentes: [
-      { slug: "mississauga-lakeshore-road", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" },
-      { slug: "mississauga-dundas-eastbound", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" }
+      { slug: 'mississauga-lakeshore-road', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' },
+      { slug: 'mississauga-dundas-eastbound', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Mississauga",
-    nombre: "Dundas Eastbound",
-    lineas: ["hazel-mcallion-purple"],
+  'mississauga-dundas-eastbound': {
+    ciudad: 'mississauga',
+    nombre: 'Dundas Eastbound',
+    lineas: ['hazel-mcallion-purple'],
     adyacentes: [
-      { slug: "mississauga-port-street", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" },
-      { slug: "mississauga-eglinton-east", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" }
+      { slug: 'mississauga-port-street', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' },
+      { slug: 'mississauga-eglinton-east', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Mississauga",
-    nombre: "Eglinton East",
-    lineas: ["hazel-mcallion-purple"],
+  'mississauga-eglinton-east': {
+    ciudad: 'mississauga',
+    nombre: 'Eglinton East',
+    lineas: ['hazel-mcallion-purple'],
     adyacentes: [
-      { slug: "mississauga-dundas-eastbound", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" },
-      { slug: "mississauga-steeles-avenue", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" }
+      { slug: 'mississauga-dundas-eastbound', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' },
+      { slug: 'mississauga-steeles-avenue', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Mississauga",
-    nombre: "Steeles Avenue",
-    lineas: ["hazel-mcallion-purple"],
+  'mississauga-steeles-avenue': {
+    ciudad: 'mississauga',
+    nombre: 'Steeles Avenue',
+    lineas: ['hazel-mcallion-purple'],
     adyacentes: [
-      { slug: "mississauga-eglinton-east", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" },
-      { slug: "mississauga-clark-boulevard", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" }
+      { slug: 'mississauga-eglinton-east', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' },
+      { slug: 'mississauga-clark-boulevard', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Mississauga",
-    nombre: "Clark Boulevard",
-    lineas: ["hazel-mcallion-purple"],
+  'mississauga-clark-boulevard': {
+    ciudad: 'mississauga',
+    nombre: 'Clark Boulevard',
+    lineas: ['hazel-mcallion-purple'],
     adyacentes: [
-      { slug: "mississauga-steeles-avenue", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" },
-      { slug: "mississauga-mcmurchy-avenue", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" }
+      { slug: 'mississauga-steeles-avenue', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' },
+      { slug: 'mississauga-mcmurchy-avenue', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Mississauga",
-    nombre: "McMurchy Avenue",
-    lineas: ["hazel-mcallion-purple"],
+  'mississauga-mcmurchy-avenue': {
+    ciudad: 'mississauga',
+    nombre: 'McMurchy Avenue',
+    lineas: ['hazel-mcallion-purple'],
     adyacentes: [
-      { slug: "mississauga-clark-boulevard", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" },
-      { slug: "mississauga-terminal-station", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" }
+      { slug: 'mississauga-clark-boulevard', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' },
+      { slug: 'mississauga-terminal-station', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' }
     ]
   },
-  {
-    ciudad: "Mississauga",
-    nombre: "Terminal Station",
-    lineas: ["hazel-mcallion-purple"],
+  'mississauga-terminal-station': {
+    ciudad: 'mississauga',
+    nombre: 'Terminal Station',
+    lineas: ['hazel-mcallion-purple'],
     adyacentes: [
-      { slug: "mississauga-mcmurchy-avenue", tiempo: 3, linea: "hazel-mcallion-purple", tipo: "lrt" }
+      { slug: 'mississauga-mcmurchy-avenue', tiempo: 3, linea: 'hazel-mcallion-purple', tipo: 'lrt' }
     ]
   }
-];
+};
